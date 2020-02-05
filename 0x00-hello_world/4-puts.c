05b758a9f248b72191ef9e9066e1b5262b34eea2
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-05 20:28:55 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main (void){
-  fputs(""Programming is like building a multilingual puzzle\n);
+  fputs("\"Programming is like building a multilingual puzzle");
   return(0);
  }
