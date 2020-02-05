9f4355721945eea2ba1fcfb1a2fd9344a490d60f
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-05 20:51:36 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints a string using puts
- * Return: 0 (Success)
- */
+  * main - prints string
+  * Return: Always 0 (Sucess)
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
