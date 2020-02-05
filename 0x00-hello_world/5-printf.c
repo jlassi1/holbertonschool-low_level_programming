573da3ca1849744d7f984021486700ffd014f83e
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-05 21:10:10 +0100

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - prints string
  * Return: Always 0 (Success)
 **/
-int main(void) {
-printf("with proper grammar, but the outcome is a piece of art\n");
-return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
