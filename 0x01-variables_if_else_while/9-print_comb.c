24c19f3b2dd79265e6697cde8f02f7008f59b87c
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-06 18:44:33 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description:prints all single digit numbers of base 10 starting from 0
+ * Return: void
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		putchar(44);
+		putchar(32);
+	}
+
+	putchar('\n');
+	return (0);
+}
