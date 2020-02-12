0afec35f91a0b0eb9f7cf41598bf45e42005c633
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-12 13:42:42 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints  Fibonacci numbers, the sum of the even-valued terms.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+long int n[34];
+int i;
+for (i = 1; i <= 33; i++)
+{
+
+if (i == 1)
+{
+n[1] = 1;
+}
+else if (i == 2)
+{
+n[2] = 2;
+}
+else
+{
+n[i] = n[i - 2] + n[i - 1];
+}
+if (n[i] % 2 == 0)
+{
+if (i != 32)
+printf("%ld, ", n[i]);
+else
+printf("%ld\n", n[i]);
+}
+
+}
+
+return (0);
+}
