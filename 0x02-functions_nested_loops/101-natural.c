2cb9c117840650d7b449f460b3126b60b8935fa8
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-12 12:01:10 +0100

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int n, i;
+n = 0;
+for (i = 1 ; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+n += i;
+}
+}
+printf("%d\n", n);
+return(0);
+
+}
