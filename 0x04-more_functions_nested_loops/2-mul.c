75593921f1f1a2f3b495e18e34c3ae8b03b271de
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-13 09:54:40 +0100

    mul and chek int and uppercase

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a : integer
+ * @b: integer
+ * Return: result
+ */
+int mul(int a, int b)
+{
+
+int c;
+c = a * b;
+return (c);
+
+
+}
