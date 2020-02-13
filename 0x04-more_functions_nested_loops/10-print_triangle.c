6e7d2f6c0234da36329becdcb0c60f8998367161
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-13 15:07:44 +0100

    10-print_tringle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ *@size: integer
+ * Return: 0.
+ */
+
+
+void print_triangle(int size)
+{
+int i, j, n;
+if (size <= 0)
+_putchar('\n');
+else
+
+for (j = 0; j < size; j++)
+{
+for (i = 0; i < size - j; i++)
+{
+_putchar(' ');
+
+}
+for (n = size - j; n <= size; n++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+
+}
