9b14351ddfee2241db08327a835f486a02bf06e4
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-13 11:35:48 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line -  draws a straight line in the terminal.
+ *@n: integer
+ * Return: 0.
+ */
+
+
+void print_line(int n)
+{
+int i;
+if (n == 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
