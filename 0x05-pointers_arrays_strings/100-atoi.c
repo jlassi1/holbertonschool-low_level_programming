13c7f673eff7914302d0b58db18029faa21f5644
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-18 14:44:47 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,12 +2,12 @@
 
 /**
  * _atoi - convert string to integer.
- * @*s: pointer.
+ * @s: pointer.
  * Return: int.
  */
 int _atoi(char *s)
 {
-int k = 0, i = 0, n = 0; 
+int k = 0, i = 0, n = 0;
 int x = 0, y = 1;
 while (s[n] != '\0')
 {
@@ -22,15 +22,13 @@ k++;
 }
 for (i = 0; i < n; i++)
 {
-if(s[i] >= '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
 x = (x * 10) + (s[i] - '0');
 }
 }
 
-x = x * y;
-return(x); 
-
-   
+x = x *y;
+return (x);
 }
 
