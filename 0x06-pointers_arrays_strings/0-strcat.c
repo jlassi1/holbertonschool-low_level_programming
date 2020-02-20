c07b52283a06eeac6c8e9ff10c5e3d3c8178948b
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-20 10:14:21 +0100

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strcat - function that concatenates two strings
+ *@dest: char
+ *@src: char
+ *Returne: p
+ */
+char *_strcat(char *dest, char *src)
+{ 
+int i = 0, j = 0, k = 0, l = 0;
+
+while (src[i] != '\0')
+i++;
+while (dest[j] != '\0')
+j++;
+
+for (k = j ; k < ( j + i); k++, l++)
+{
+dest[k] = src[l];
+}
+
+return (dest);
+}
