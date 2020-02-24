0a3a9a7c36155be552e26c21d091dbb240591680
jlassi1 <khawlajlassi1990@gmail.com>
2020-02-24 20:51:13 +0100

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_chessboard - function that prints the chessboard.
+ *@a: char
+ *Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+int x, sum = 0, sum_inv = 0;
+
+for (x = 0, sum = 0; x < (size * size) ; x += size + 1)
+sum += a[x];
+
+for (x = size - 1, sum_inv = 0; x <= (size * size) - 2; x += size - 1)
+sum_inv += a[x];
+
+printf("%d, %d\n", sum, sum_inv);
+}
