bb564282891c08e650405e2abce995c283c2fba9
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-01 11:21:20 +0100

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main function
+ *@argc: size of array of command
+ *@argv: array of command
+ *Return:  0
+ */
+int main(int argc, char *argv[])
+{
+int x = 0, i;
+argv[argc] = '\0';
+for (i = 1; i < argc ; i++)
+{
+
+x += atoi(argv[i]);
+
+
+
+if (!(atoi(argv[i])))
+break;
+
+}
+if (i == argc)
+
+printf("%d\n", x);
+
+else
+printf("Error\n");
+return (0);
+}
+
