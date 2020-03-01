705dc63c3ef219da0feb14586e386fadf0fd4582
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-01 23:05:00 +0100

    last commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  *main - main function
  *@argc: size of array of command
@@ -11,17 +13,17 @@ int main(int argc, char *argv[])
 {
 unsigned int x = 0;
 int i;
-argv[argc] = '\0';
+/*argc = strlen(*argv);*/
 for (i = 1; i < argc ; i++)
 {
 
 x += atoi(argv[i]);
 
+
 if (!(atoi(argv[i])))
 break;
-
 }
-if (i == argc )
+if (i == argc)
 
 printf("%d\n", x);
 
