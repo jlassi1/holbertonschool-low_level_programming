2e03d85b4c0c771585d787db4bf259dd87762bb4
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-01 07:51:42 +0100

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ *main - main function
+ *@argc: size of array of command
+ *@argv: array of command
+ *Return:  0
  */
 int main(int argc, char *argv[])
 {
