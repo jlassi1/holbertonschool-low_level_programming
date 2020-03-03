d494749ebcf8b5491bdbe2743816d8cc160c3348
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-03 15:57:02 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@ char *str_concat(char *s1, char *s2)
 {
 char *array;
 unsigned int i = 0, j = 0, k, l = 0, x = 0;
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL && s2 == NULL)
 return (NULL);
 
 while (s1[j] != '\0')
