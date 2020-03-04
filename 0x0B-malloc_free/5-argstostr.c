710ad6695a267d7f99fea5c6745754a5ea586d35
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-04 22:44:23 +0100

    5-argstostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *argstostr- concatenates all the arguments of your program.
+ *argstostr - concatenates all the arguments of your program.
  *@av : pointer of char
  *@ac : integer
- *Return: pointer 
+ *Return:pointer of char
  */
 char *argstostr(int ac, char **av)
 {
 char *a = NULL;
-int i = 0, j = 0, lenth = 0, x = 0; x = 0;
+int i = 0, j = 0, lenth = 0, x = 0;
+
 if (ac == 0 || av == NULL)
-return (NULL);
+	return (NULL);
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
+{
 lenth++;
+}
 lenth++;
+}
 
-}                                                                                              
+a = malloc((lenth + 1) * sizeof(char));
 
-
-if (av == NULL)
+if (a == NULL)
 return (NULL);
-
-for (j = 0; j <= ac; j++)
-a = malloc(ac* sizeof(char));
 
 for (i = 0; i < ac; i++)
 {
