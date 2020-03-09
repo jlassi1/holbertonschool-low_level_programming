7119a018589e18236292402273fca83d1a75b20c
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-09 13:18:49 +0100

    new dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,11 @@ float age;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+struct dog
+{
+char *name;
+char *owner;
+float age;
+}dog_t;
 
 #endif
