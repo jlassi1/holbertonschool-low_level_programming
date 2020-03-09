d9b5320abc89615751c421fbf77d281cc8763b5f
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-09 13:14:48 +0100

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,16 @@ if (d == NULL)
 return;
 
 if (d->name == NULL)
-printf("Name : (nil)\n");
+printf("Name: (nil)\n");
 else
-printf("Name : %s\n", d->name);
+printf("Name: %s\n", d->name);
 
-printf("Age : %f\n", d->age);
+printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
-printf("Owner : (nil)\n");
+printf("Owner: (nil)\n");
 else
-printf("Owner : %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 
 
 }
