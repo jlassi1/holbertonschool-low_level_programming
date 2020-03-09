e2fef32a8cccb7d6c5b6d26af4717a7051808cda
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-09 18:55:24 +0100

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *new_dog-creates a new dog
+  *@name: name of dog
+  *@owner: owner of dog
+  *@age: age of dog
+  *Return:
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+int i = 0, j = 0, x, y;
+char *new_name;
+char *new_owner;
+
+if (name == NULL || owner == NULL)
+return (NULL);
+
+d = malloc(sizeof(dog_t));
+
+if (d ==  NULL)
+return (NULL);
+
+while(name[i])
+    ;
+new_name = malloc(sizeof(char) * (i + 1));
+
+if (new_name == NULL)
+{
+free(d);
+return(NULL);
+}
+
+for (x = 0; x < i; i++)
+new_name[x] = name[x];
+new_name[x] = '\0';
+
+while(owner[j])
+    ;
+
+new_owner = malloc(sizeof(char) * j + 1);
+if (new_owner == NULL)
+{
+free(new_name);
+free(d);
+return(NULL);
+}
+for (y = 0; y < j; j++)
+new_owner[y] = owner[y];
+new_owner[y] = '\0';
+
+d->name = new_name;
+d->age = age;
+d->owner = new_owner;
+return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,4 +28,7 @@ char *owner;
 float age;
 } dog_t;
 
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 #endif
