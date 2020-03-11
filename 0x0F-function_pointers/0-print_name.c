a2fe6e31538f375e8caf346beedf3cd92c19792b
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-11 10:54:17 +0100

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+  *print_name - print name
+  *@name: pointer of name
+  *@f: pointer to function
+  * Return: void
+  */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL)
+return;
+
+f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+
+
+
+
+
+
+
+#endif
