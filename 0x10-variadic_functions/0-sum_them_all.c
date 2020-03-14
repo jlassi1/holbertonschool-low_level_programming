d2e531cf544cd57906c25b09e5e060a5772f08bc
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-14 12:41:09 +0100

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+va_list arg;
+
 unsigned int i, sum = 0;
+
 if (n == 0)
 return (0);
 
-va_list arg;
+
 va_start(arg, n);
 for (i = 0; i < n; i++)
 
