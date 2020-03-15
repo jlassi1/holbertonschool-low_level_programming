7166b1458c16c593286bd51007efd5bcfcd0a662
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-15 15:11:43 +0100

    fixed for checker

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,86 +1,52 @@
-#include <stdlib.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdlib.h>
 /**
-  *print_int - print integer
-  *@arg: argument of compare
-  */
-void print_int(va_list arg)
-{
-printf("%d", va_arg(arg, int));
-}
-/**
-  *print_float - print float
-  *@arg: argument of compare
-  */
-void print_float(va_list arg)
-{
-printf("%f", va_arg(arg, double));
-}
-/**
-  *print_string - print string
-  *@arg: argument of compare
-  */
-void print_string(va_list arg)
-{
-char *s;
-s = va_arg(arg, char *);
-if (s == NULL)
-s = "(nil)";
-printf("%s", s);
-}
-/**
-  *print_char - print character
-  *@arg: argument of compare
-  */
-void print_char(va_list arg)
-{
-
-printf("%c", va_arg(arg, int));
-}
-/**
- * print_all -  prints anything, followed by a new line.
- * @format: type of argument
- * Return: void
+ * print_all - a variadic function that prints any given input
+ * @format: a list of eligible types
+ * Return:
  */
-
 void print_all(const char * const format, ...)
 {
 va_list arg;
-
-unsigned int k = 0, l = 0;
-char *coma_0 = "";
-char *coma_1 = ", ";
-
-format_t type[] = {
-{'c', print_char},
-{'i', print_int},
-{'f', print_float},
-{'s', print_string},
-{'0', NULL}
-};
-
+int i = 0, j = 0;
+char *s;
 
 va_start(arg, format);
-
-while (format[k] != '\0' && format != NULL)
-{
-while (type[l].x != '0')
-{
-if (format[k] == type[l].x)
-{
-printf("%s", coma_0);
-(type[l].print_format)(arg);
-coma_0 = coma_1;
-break;
-}
-l++;
-}
-k++;
-l = 0;
-}
-va_end(arg);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				j = 1;
+				break;
+			case 'i':
+				printf("%i", va_arg(arg, int));
+				j = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				j = 1;
+				break;
+			case 's':
+				s = va_arg(arg, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				j = 1;
+				break;
+			default:
+				j = 0;
+		}
+		if ((j) && (format[i + 1] != '\0'))
+			printf(", ");
+	i++;
+	}
 printf("\n");
-
+va_end(arg);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,25 +6,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_char(va_list arg);
-/**
-  *struct format-struct type
-  *struct format_t-new name for the type struct format
-  *@x: indection of type
-  *@print_format: function that print format
-  */
-typedef struct format
-{
-char *x;
-void (*print_format)();
-} format_t;
-
-
-
 void print_all(const char * const format, ...);
 
 
