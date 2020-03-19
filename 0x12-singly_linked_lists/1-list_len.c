58822328e262a3d2fc96fcf85cd332c6ae71f0fc
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-19 12:03:13 +0100

    checkk betty for 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,8 +3,8 @@
 /**
   *list_len - give the number of elements in a linked list_t list
   *@h: pointer to const structure
-  * 
-  *Return: nbr of element 
+  *
+  *Return: nbr of element
   */
 size_t list_len(const list_t *h)
 {
@@ -15,8 +15,5 @@ while (h->next != NULL)
 h = NEXT;
 n++;
 }
-
-
 return (n);
-
 }
