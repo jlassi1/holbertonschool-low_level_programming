9193c0d6ba122040701ff8d36440bc0420b6caf8
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-20 19:51:01 +0100

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,15 +25,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
- int main(void);
+
 void beforeMain(void) __attribute__((constructor));
-
-typedef struct list_func
-{
-char *str;
-int (*func)();
-struct list_s *next;
-} list_f;
-
 
 #endif
