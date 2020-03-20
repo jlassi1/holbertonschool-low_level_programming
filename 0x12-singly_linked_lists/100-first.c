fd289059b8bebfff1ea6e664a3de9c9a933faa92
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-20 19:46:10 +0100

    write function execute before main()

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  *beforeMain - function that come before main
+  *Return: void
+  */
+
+void beforeMain(void)
+{
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}
