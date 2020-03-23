63a19d3d15475b320a36b8d55cdc34a446f8d725
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-23 20:45:52 +0100

    fixe condition index == 0

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int n = 0;
 while (head != NULL)
 {
-head = head->next;
-n++;
 if (n == index)
 return (head);
-
+head = head->next;
+n++;
 }
 
 return (NULL);
