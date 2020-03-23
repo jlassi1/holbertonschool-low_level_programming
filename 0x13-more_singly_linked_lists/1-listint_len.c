fd8c66eca8f421cec801355a3b03cbd0967064a1
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-23 11:07:50 +0100

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  *listint_len - count the number of elements in a linked
+  *@h: nodes of linkes lists
+  *
+  *Return: nbr of nosdes
+  */
+size_t listint_len(const listint_t *h)
+{
+size_t n = 0;
+
+while (h != NULL)
+{
+
+h = h->next;
+n++;
+}
+return (n);
+
+}
