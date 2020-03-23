f48b44ea830b129ced9f6b6df4a02118754d54a7
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-23 11:01:16 +0100

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  *print_listint - print all the elements of a listint_t
+  *@h: nodes of linked list
+  *Return: nbr of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+size_t n = 0;
+
+while (h != NULL)
+{
+printf("%u\n", h->n);
+h = h->next;
+n++;
+}
+return (n);
+}
