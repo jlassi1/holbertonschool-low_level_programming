a324251e410decfc619e794174c4163844dc13fa
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-26 14:22:33 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,26 +7,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-        if (!b)
-        return (0);
-  while(*b)
-  {
-          if (*b != 48 && *b != 49)
-          return (0);
-    num = ((*b)-'0') + num*10;
-    b++;   
-  }
+unsigned int num = 0, dec = 0;
+int  rem, binary = 1;
+if (!b)
+return (0);
+while (*b)
+{
+if (*b != 48 && *b != 49)
+return (0);
+num = ((*b) - '0') + num * 10;
+b++;
+}
 
-  int dec = 0, i = 0, rem, binary = 1;
-    while (num != 0)
-     {
-        rem = num % 10;
-        num /= 10;
-        dec += rem * binary;
-        ++i;
-        binary *= 2;
-    }
+while (num != 0)
+{
+rem = num % 10;
+num /= 10;
+dec += rem *binary;
+binary *= 2;
+}
 
-   return (dec);
+return (dec);
 }
