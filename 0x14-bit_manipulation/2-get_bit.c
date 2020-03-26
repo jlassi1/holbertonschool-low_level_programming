e40bea08344658891569cc09a7aae28159954d3b
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-26 17:22:40 +0100

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *get_bit- get the value of a bit at a given index.
+ *@n: unsigned integer
+ *@index: of bit to find
+ *Return: the value of the bit at index index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (!n)
+return (-1);
+if (n > 1 && n >> index)
+{
+n = n >> index;
+if (n & 1)
+return (1);
+else
+return (0);
+}
+return (-1);
+}
