bcfb6e5ead5a5f786d3ab0bcc96c3ef30ee511dc
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-26 18:11:09 +0100

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,11 @@ int get_bit(unsigned long int n, unsigned int index)
 if (index > sizeof(unsigned long int) * 8)
 return (-1);
 
-if (n >> index)
-{
+
 n = n >> index;
 if (n & 1)
 return (1);
 else
 return (0);
-}
-return (-1);
+
 }
