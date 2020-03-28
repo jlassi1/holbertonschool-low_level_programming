ee1b238f3b8df8fef5b530c445e4fda193685c38
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-29 00:26:29 +0100

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,28 +3,30 @@
  *free_listint_safe- free linked list
  *@h: pointer to the hear of linked list
  *
- *Return: nbr of node
+ *Return: the size of the list that was free’d
  */
 size_t free_listint_safe(listint_t **h)
 {
 listint_t *temp, *node;
 size_t i = 0;
 node = *h;
+temp = *h;
 if (h == NULL)
 return (i);
 	while (node != NULL)
 	{
 
-		temp = node->next;
-		free(node);
-		node = temp->next;
-			i++;
-		if (temp <= node)
+		if (temp <= node->next)
 		{
 			free(temp);
 			i++;
 			break;
 		}
+
+		temp = node->next;
+		free(node);
+		node = temp->next;
+			i++;
 	}
 *h = NULL;
 return (i);
