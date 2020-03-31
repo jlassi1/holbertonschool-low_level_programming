49594c1027bbec220a8eb2b7926064aeb180e81d
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-31 17:24:42 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,31 @@
  *Return: 1 on success, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 
+ssize_t fw;
+int fo, i = 0;
+
+		if (filename == NULL)
+			return (-1);
+
+	fo = open(filename, O_RDWR | O_CREAT, 0600);
+		if (fo == -1)
+			return (-1);
+
+		if (text_content == NULL)
+			text_content = "";
+		while (text_content[i] != '\0')
+			i++;
+
+
+	fw = write(fo, text_content, i);
+		if (fw == -1)
+			return (-1);
+
+
+	close(fo);
+
+return (1);
 }
