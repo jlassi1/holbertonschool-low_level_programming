f8234b457772d69639e3f92d2e767fb424409c33
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-31 14:42:12 +0100

    try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fo, fr, fw;
+ssize_t fo, fr, fw;
 
 void *buf;
 
@@ -22,19 +22,24 @@ void *buf;
 	fo = open(filename, O_RDWR);
 		if (fo == -1)
 		{
+			free(buf);
 			return (0);
 		}
 	fr = read(fo, buf, letters);
 		if (fr == -1)
 		{
+			free(buf);
 			return (0);
 		}
 
 	fw = write(1, buf, letters);
 		if (fw == -1)
 		{
+			free(buf);
 			return (0);
 		}
+		if (fw != fr)
+		fw = fr + 1;
 	free(buf);
 	close(fo);
 
