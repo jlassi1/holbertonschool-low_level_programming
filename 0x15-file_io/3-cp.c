e58afcd9c70001c6ab6fc2a4b387b68c4be890f9
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-01 12:05:17 +0100

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+/**
+ *file_from - message d'error for file1
+ *@f1: file frome what to copie
+ */
+void file_from(char *f1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f1);
+exit(98);
+}
+/**
+ *file_to- messege d'error for file2
+ *@f2: new file
+ */
+void file_to(char *f2)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f2);
+exit(99);
+}
+/**
+ *main- creat cp commande
+ *@ac: number of argument
+ *@av: argument
+ *Return: 0
+ */
+int main(int ac, char *av[])
+{
+int y, x, fo, fd, rd = 1024, wr;
+char buf[1024];
+
+if (ac != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+fd = open(av[1], O_RDWR);
+if (fd == -1)
+file_from(av[1]);
+fo = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+if (fo == -1)
+file_to(av[2]);
+while (rd == 1024)
+{
+rd = read(fd, buf, 1024);
+if (rd == -1)
+file_from(av[1]);
+wr = write(fo, buf, rd);
+if (wr == -1)
+file_to(av[2]);
+}
+x = close(fd);
+if (x == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+
+y = close(fo);
+if (y == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fo), exit(100);
+return (0);
+}
