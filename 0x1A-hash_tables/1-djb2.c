7cb17ab2a2dded0d2762589c22937a965901d7aa
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-29 15:24:28 +0100

    check_betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,13 +7,14 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = (hash * 33) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = (hash * 33) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
+
