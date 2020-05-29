b2f5de0e2c0ca570c312467734fcefb71b816ce0
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-29 15:45:26 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,24 +10,24 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new, *tmp;
 	unsigned long int index;
 
 	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
 
-
-	while (ht->array[index])
+	while (tmp)
 	{
-		if (strcmp(key, ht->array[index]->key) == 0)
+		if (strcmp(key, tmp->key) == 0)
 		{
-			ht->array[index]->value = strdup(value);
+			tmp->value = strdup(value);
 			return (1);
 		}
 		else
-			ht->array[index] = ht->array[index]->next;
+			tmp = tmp->next;
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
