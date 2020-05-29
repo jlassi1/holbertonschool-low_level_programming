77c8004f69aef2afb0d34b52c9aa6455bb779502
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-29 15:18:58 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,22 +10,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 
 hash_table_t *tab;
+unsigned long int x;
 
 	tab = malloc(sizeof(hash_table_t));
 		if (tab == NULL)
 			return (NULL);
 
-	hash_node_t **ar = malloc(sizeof(hash_node_t *) * size);
-		if (ar == NULL)
+	tab->array = malloc(sizeof(hash_node_t *) * size);
+		if (tab->array == NULL)
 		{
 			free(tab);
 			return (NULL);
 		}
 
-	tab->array = ar;
 	tab->size = size;
+	for (x = 0; x < size; x++)
+		tab->array[x] = NULL;
 
 return (tab);
-
-}
 
