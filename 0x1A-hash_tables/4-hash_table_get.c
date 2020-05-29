eab681c194b3a294e41e51e14894d9d842ce9e0d
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-29 15:53:33 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 unsigned long int index = key_index((const unsigned char *)key, ht->size);
 hash_node_t *node = ht->array[index];
 
-	while (node)
+		if (ht == NULL || key == NULL)
+			return (NULL);
+
+	while (node != NULL)
 	{
-		if (strcmp(key, node->key) == 0)
+		if (strcmp(node->key, key) == 0)
 		{
 			return (node->value);
 		}
