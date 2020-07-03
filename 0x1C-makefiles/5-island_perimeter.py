2ffc522137eb863a8bc3687d239b98dcef18190c
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-03 19:02:17 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+island perimenter
+"""
+
+
+def island_perimeter(grid):
+    """function returns the perimeter of the island described in grid"""
+    max_width = 0
+    width = 0
+    heigth = 0
+    for i in grid:
+        for j in i:
+            if j == 1:
+                width += 1
+        if width != 0:
+            heigth += 1
+        if max_width < width:
+            max_width = width
+        width = 0
+    return (max_width + heigth) * 2
