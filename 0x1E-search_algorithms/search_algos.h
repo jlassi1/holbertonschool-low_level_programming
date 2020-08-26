7dcbbf05d60a41016f51cf627b51ada5be5b1910
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-26 21:12:39 +0100

    first

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef __SEARCH__
+#define __SEARCH__
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
